91e70592efce30f93a158f4aa494525b7a072a88
cascandaliato <8927157+cascandaliato@users.noreply.github.com>
2020-12-22 15:34:02 +0100

    add maxAge parameter and error handling

--- pages/api/cache.ts
+++ pages/api/cache.ts
@@ -3,25 +3,37 @@ import httpAdapter from 'axios/lib/adapters/http';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const {
-    query: { url },
-  } = req;
+  try {
+    const {
+      query: { url, maxAge: maxAgeOverride },
+    } = req;
 
-  if (!url || typeof url === 'object')
-    return res.status(400).json({
-      error:
-        'specify a single url parameter, e.g. https:/voulez-vous-cacher-avec-moi.vercel.app/api/cache?url=',
+    if (!url || typeof url === 'object')
+      return res
+        .status(400)
+        .send(
+          'Specify a single url parameter, e.g. https:/voulez-vous-cacher-avec-moi.vercel.app/api/cache?url=https://www.rspcasa.org.au/wp-content/uploads/2019/01/Adopt-a-cat-or-kitten-from-RSPCA.jpg'
+        );
+
+    const axiosResponse = await axios.get(url, {
+      responseType: 'stream',
+      adapter: httpAdapter,
     });
 
-  const axiosResponse = await axios.get(url, {
-    responseType: 'stream',
-    adapter: httpAdapter,
-  });
+    const maxAge =
+      typeof maxAgeOverride === 'string' && /^\d+$/.test(maxAgeOverride)
+        ? Number(maxAgeOverride)
+        : 60;
 
-  res.setHeader(
-    'cache-control',
-    's-maxage=1, stale-while-revalidate, max-age=60'
-  );
-  res.setHeader('content-type', axiosResponse.headers['content-type']);
-  axiosResponse.data.pipe(res);
+    res.setHeader(
+      'cache-control',
+      `s-maxage=1, stale-while-revalidate, max-age=${maxAge}`
+    );
+
+    res.setHeader('content-type', axiosResponse.headers['content-type']);
+
+    axiosResponse.data.pipe(res);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 };
